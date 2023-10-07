ce4558fac34ed4872c13a20d217381e76e0c78a4
Edward-Kabue <ekabue0@gmail.com>
2023-10-07 16:05:23 +0300

    Add appointments resource and enums

---
+++ app/Filament/Resources/AppointmentResource.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use Filament\Forms;
+use Filament\Tables;
+use Filament\Forms\Form;
+use Filament\Tables\Table;
+use App\Models\Appointment;
+use App\Enums\AppointmentStatus;
+use Filament\Resources\Resource;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use App\Filament\Resources\AppointmentResource\Pages;
+use App\Filament\Resources\AppointmentResource\RelationManagers;
+
+class AppointmentResource extends Resource
+{
+    protected static ?string $model = Appointment::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\Section::make([
+                    Forms\Components\Select::make('pet_id')
+                        ->relationship('pet', 'name')
+                        ->searchable()
+                        ->preload()
+                        ->required(),
+
+                    Forms\Components\TextInput::make('description')
+                        ->required(),
+                    Forms\Components\Select::make('status')
+                        ->native(false)
+                        ->options(AppointmentStatus::class)
+                        //only visible when editing not creating a record
+                        ->visibleOn(Pages\EditAppointment::class)
+                ])
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('pet.name')
+                    ->searchable()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('description')
+                    ->searchable()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('status')
+                    ->badge()
+                    ->sortable()
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\Action::make('Confirm')
+                ->action(function (Appointment $record) {
+                    $record->status = AppointmentStatus::Confirmed;
+                    $record->save();
+                })
+                ->visible(fn (Appointment $record) => $record->status == AppointmentStatus::Created)
+                ->color('success')
+                ->icon('heroicon-o-check'),
+                Tables\Actions\Action::make('Cancel')
+                    ->action(function (Appointment $record) {
+                        $record->status = AppointmentStatus::Canceled;
+                        $record->save();
+                    })
+                    ->visible(fn (Appointment $record) => $record->status != AppointmentStatus::Canceled)
+                    ->color('danger')
+                    ->icon('heroicon-o-x-mark'),
+                
+                
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ])
+            ->emptyStateActions([
+                Tables\Actions\CreateAction::make(),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListAppointments::route('/'),
+            'create' => Pages\CreateAppointment::route('/create'),
+            'edit' => Pages\EditAppointment::route('/{record}/edit'),
+        ];
+    }
+}
--- app/Models/Appointment.php
+++ app/Models/Appointment.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Enums\AppointmentStatus;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -9,6 +10,9 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 class Appointment extends Model
 {
     use HasFactory;
+    protected $casts = [
+        'status' => AppointmentStatus::class,
+    ];
     public function pet(): BelongsTo
     {
         return $this->belongsTo(Pet::class);
